309a9b7c7a5eaf978a3cbf6bb14d4d5b84c903dd
RaselDev28 <rasel.dev28@gmail.com>
2026-03-08 19:17:28 +0600

    licuid icon

--- src/Componants/Navbar/Navbar.jsx
+++ src/Componants/Navbar/Navbar.jsx
@@ -1,41 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from './link';
-const navigationData=[
-  {
-    "id": 1,
-    "name": "Home",
-    "path": "/"
-  },
-  {
-    "id": 2,
-    "name": "About",
-    "path": "/about"
-  },
-  {
-    "id": 3,
-    "name": "Services",
-    "path": "/services"
-  },
-  {
-    "id": 4,
-    "name": "Contact",
-    "path": "/contact"
-  },
-  {
-    "id": 5,
-    "name": "Blog",
-    "path": "/blog"
-  }
+import { Menu, X } from 'lucide-react';
+const navigationData = [
+    {
+        "id": 1,
+        "name": "Home",
+        "path": "/"
+    },
+    {
+        "id": 2,
+        "name": "About",
+        "path": "/about"
+    },
+    {
+        "id": 3,
+        "name": "Services",
+        "path": "/services"
+    },
+    {
+        "id": 4,
+        "name": "Contact",
+        "path": "/contact"
+    },
+    {
+        "id": 5,
+        "name": "Blog",
+        "path": "/blog"
+    }
 ]
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
+    const links = navigationData.map(route => <Link key={route.id} route={route}></Link>)
     return (
-        <nav>
-            <ul className='flex'>
+        <nav className='flex justify-between mx-10'>
+            <span className='flex' onClick={() => setOpen(!open)}>
+                {open ?
+                    <X className='md:hidden'></X> :
+                    <Menu className='md:hidden'></Menu>
+                }
+                <ul className='md:hidden'>
+                    {links}
+                </ul>
+                <h2 className='ml-4'>My Navbar</h2>
+            </span>
+            <ul className='md:flex hidden' >
                 {
-                    navigationData.map(route=> <Link key={route.id} route={route}></Link>)
+                    links
                 }
             </ul>
+            <button>Sign In</button>
         </nav>
     );
 };
